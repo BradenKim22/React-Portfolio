a9c1de7d2a39487c622fc0a6df2e052c60d55bb6
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-05-19 16:58:31 -0600

    components are set up to work

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,33 @@ import React from "react";
 import Nav from "./Nav";
 import profileImage from "./assets/profile-image.png";
 
+const styles = {
+  header: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: "11vh",
+    margin: "3.5% 0 3.5% 0",
+    backgroundColor: "orange",
+  },
+  profile: {
+    height: "200%",
+    borderRadius: 50,
+  },
+  name: {
+    fontSize: "300%",
+    color: "black",
+  },
+};
+
 function Header() {
-    return (
-        <header>
-            // add the image here and the Nav Here
-            <Nav />
-        </header>
-    )
+  return (
+    <header style={styles.header}>
+      <img style={styles.profile} src={profileImage} />
+      <h1 style={styles.name}>Braden Kim</h1>
+      <Nav />
+    </header>
+  );
 }
 
 export default Header;
