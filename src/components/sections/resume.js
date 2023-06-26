93cc04204a2c1e7d6dc61f63b17caed02832a3cb
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-06-26 15:10:04 -0600

    minimal resume section figuring it out..

--- src/components/sections/resume.js
+++ src/components/sections/resume.js
@@ -1,6 +1,13 @@
 import React from "react";
-// My Resume and List of Proficiencies
+import { Document, Page } from "react-pdf";
+import ResumePDF from "../assets/braden-kim-resume.pdf";
 
+// My Resume and List of Proficiencies
 export default function Resume() {
-  return <p>This is Resume</p>;
+  return (
+    <div>
+      <Document file={ResumePDF}></Document>
+      <a href={ResumePDF} download >Download Resume</a>
+    </div>
+  );
 }
